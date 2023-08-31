2bc3e2a338ab7a260be6e3329c16cd0d24402dc6
Justin Kempton <jnkempton@gmail.com>
2023-08-31 22:48:49 +1200

    formating

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -19,8 +19,7 @@ export function NewTodoForm({ onSubmit }) {
           New Item
         </label>
         <input
-          className="w-full rounded-md border-2 border-blue-400 bg-blue-400/50 py-0.5 pl-1.5 text-black shadow 
-          shadow-blue-400/40 outline-none transition-all placeholder:text-blue-400 focus:shadow-blue-400/90 dark:text-white"
+          className="w-full rounded-md border-2 border-blue-400 bg-blue-400/50 py-0.5 pl-1.5 text-black shadow shadow-blue-400/40 outline-none transition-all placeholder:text-blue-400 focus:shadow-blue-400/90 dark:text-white"
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
           type="text"
@@ -28,10 +27,7 @@ export function NewTodoForm({ onSubmit }) {
           placeholder="add an item"
         />
       </div>
-      <button
-        className="select-none rounded-md border-2 border-blue-500 bg-blue-500/50 py-0.5 font-semibold
-        text-blue-500 shadow shadow-blue-500/40 transition-all hover:bg-blue-500/60 hover:shadow-blue-500/90"
-      >
+      <button className="select-none rounded-md border-2 border-blue-500 bg-blue-500/50 py-0.5 font-semibold text-blue-500 shadow shadow-blue-500/40 transition-all hover:bg-blue-500/60 hover:shadow-blue-500/90">
         Add
       </button>
     </form>
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -2,12 +2,9 @@ export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
   return (
     <li className="flex" key={id}>
       <label
-        className={`mr-2 flex-1 cursor-pointer select-none rounded-md border-2 px-2.5 py-0.5
-        ${
+        className={`${
           completed && "line-through"
-        } bg-gray-50 text-black shadow shadow-gray-400/40
-        transition-all hover:bg-gray-100/80
-        hover:shadow-gray-400/90 dark:bg-white/30 dark:text-white dark:hover:bg-white/40`}
+        } mr-2 flex-1 cursor-pointer select-none rounded-md border-2 bg-gray-50 px-2.5 py-0.5 text-black shadow shadow-gray-400/40 transition-all hover:bg-gray-100/80 hover:shadow-gray-400/90 dark:bg-white/30 dark:text-white dark:hover:bg-white/40`}
       >
         <input
           className="mr-1.5 cursor-pointer align-baseline"
@@ -18,8 +15,7 @@ export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
         {title}
       </label>
       <button
-        className="h-min select-none self-center rounded-md border-2 border-red-500 bg-red-500/50 px-2.5 py-0.5 text-red-500
-        shadow shadow-red-500/40 transition-all hover:bg-red-500/60 hover:shadow-red-500/90"
+        className="h-min select-none self-center rounded-md border-2 border-red-500 bg-red-500/50 px-2.5 py-0.5 text-red-500 shadow shadow-red-500/40 transition-all hover:bg-red-500/60 hover:shadow-red-500/90"
         onClick={() => deleteTodo(id)}
       >
         Delete
